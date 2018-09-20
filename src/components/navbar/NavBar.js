5d1a2b6fca12ce9e3e9feef33b7a6bd7d89ad9ac
Ryan Grosser <ryan.grosser91@gmail.com>
2018-09-20 14:57:07 +1000

    Added Navbar component

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class NavBar extends Component {
+    render() {
+        return (
+            <div>
+                <p className="App-intro">
+                Roster
+                </p>
+            </div>
+        )
+    }
+}
+
+export default NavBar
