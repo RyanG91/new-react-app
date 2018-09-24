2ff17d574d1f59820f512dc566df062b6c2c9c1b
Ryan Grosser <ryan.grosser91@gmail.com>
2018-09-24 11:12:16 +1000

    Added some css stlying but still needs a little work

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <NavBar />
-          <img src={logo} className="App-logo" alt="logo" />
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
