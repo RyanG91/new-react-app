2f03408f440c0013ee0de3ff995b61ffabdc2ec8
Ryan Grosser <ryan.grosser91@gmail.com>
2018-10-08 15:41:18 +1000

    Added two history catagories

---
+++ src/components/geography/GeographyQuiz.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import '../../stylesheets/App.css';
+
+class Geography extends Component {
+    render() {
+        return (
+            <div>
+                <p>The geography page, please select one of the topics below</p>                
+            </div>
+        )
+    }
+}
+
+export default Geography
---
+++ src/components/history/roman/RomanQuiz.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import '../../../stylesheets/App.css';
+
+class Roman extends Component {
+    render() {
+        return (
+            <div>
+                <p>The Roman page, please select one of the topics below</p>                
+            </div>
+        )
+    }
+}
+
+export default Roman
